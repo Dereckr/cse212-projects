6efcba8b1e184981e52432e8ab404aaec8328886
Dereckr <dereckromos@gmail.com>
2025-07-16 19:44:31 -0400

    finish code week 2

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,23 +6,48 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Scenario: FredLow, JohnHigh, SueMedium will be enqueued. Where JohnHigh have the highest priority then, SueMedium and then FredLow
+    // Expected Result: The Item that should be dequed first is JohnHigh
+    // Defect(s) Found: The test doesnt end, I thought it might be because it was not removing anything.
+    // I added  _queue.RemoveAt(highPriorityIndex); to the dequeue function and also a -1 in the for loop
+
+
+    public void TestEnqueueAndDequeue()
     {
+
         var priorityQueue = new PriorityQueue();
-        // Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("FredLow", 1);
+        priorityQueue.Enqueue("JohnHigh", 6);
+        priorityQueue.Enqueue("SueMedium", 3);
+
+        var result = new List<string>();
+        while (priorityQueue.Length > 0)
+        {
+
+            result.Add(priorityQueue.Dequeue());
+        }
+        var expectOrder = new List<string> { "JohnHigh", "SueMedium", "FredLow" };
+
+        CollectionAssert.AreEqual(expectOrder, result);
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    // Scenario: FredLow, JohnHighFirst, SueMediumSecond will be enqueued. Where JohnHighFirst was enqueued before Sue that has the same priority, JohnHighFirst should be dequeued.
+    // Expected Result: JohnHighFirst should be dequeued first
+    // Defect(s) Found: It came first Sue that had the same priority as John, it should be John because it was in the queue first.
+    //I found that this if statement should be changed from >= to this > so it just takes in account priority if the priority is higher and not equal.
+    public void TestSamePriority()
     {
+
         var priorityQueue = new PriorityQueue();
-        // Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("FredLow", 1);
+        priorityQueue.Enqueue("JohnHighFirst", 6);
+        priorityQueue.Enqueue("SueHighSecond", 6);
+
+        var result = priorityQueue.Dequeue();
+
+        Assert.AreEqual("JohnHighFirst", result);
     }
 
     // Add more test cases as needed below.
